fc07fcaec01622eb1998631644f72dbacafb497c
RiteshopShrivastava <87560855+RiteshopShrivastava@users.noreply.github.com>
2021-07-17 13:16:34 +0530

    Add files via upload

--- Quakes dataset visualization.R
+++ Quakes dataset visualization.R
@@ -6,39 +6,46 @@ head(quakes)
 head(quakes,15)
 tail(quakes)
 plot(quakes)
-plot(quakes$lat, main='Lattitude visualization',type="l",col= 'red')
+plot(quakes$depth, main='Depth visualization',type="l",col= 'red')
 
 
 
-barplot(quakes$stations, main = 'Barplot for stations', col= 'red',horiz = T,axes=T)
+barplot(quakes$mag, main = 'Barplot for Magnitude', col= 'red',horiz = T,axes=T)
 
 
-hist(quakes$lat, main = 'histogram of lat', xlab = 'lattitude',
-     ylab = 'lattitude frequency', col = 'red')
+hist(quakes$depth, main = 'histogram of Depth', xlab = 'depth',
+     ylab = 'Depth frequency', col = 'red')
 
 
-boxplot(quakes$lat, main = ' Single Boxplot for lattitude')
-boxplot(quakes[,1:3], main= ' Multiple boxplot',col = 'red')
+boxplot(quakes$depth, main = ' Single Boxplot for Depth')
+boxplot(quakes[,1:4], main= ' Multiple boxplot',col = 'red')
 
 
-boxplot.stats(quakes$lat)$out
+boxplot.stats(quakes$depth)$out
 par(mfrow=c(4,3), mar=c(2,5,2,1), las=1, bty="n" )
 #par(mfrow=c(3,3), mar=c(2,5,2,1), las=1,bty='n')
 #par(mfrow=c(3,3),mar=c(2,5,2,1),  las=1, bty="n")
 
-plot(quakes$lat)
-plot(quakes$lat,quakes$long)
+plot(quakes$depth)
+plot(quakes$depth,quakes$mag)
 plot(quakes$depth, type='l')
-plot(quakes$long, type = 'l')
-plot(quakes$depth, type = 'l')
-barplot(quakes$lat, main = 'Barplot for Latitude', col= 'green',horiz = T,axes=T)
-hist(quakes$lat)
-hist(quakes$long)
-boxplot(quakes$lat, main = ' Single Boxplot for lattitude')
-boxplot(quakes[,1:3], main= ' Multiple boxplot',col = 'red')
+plot(quakes$mag, type = 'l')
 
-var(quakes)
-skewness(quakes$lat,na.rm=T)
-kurtosis(quakes$lat)
+barplot(quakes$depth, main = 'Barplot for depth', col= 'green',horiz = T,axes=T)
+hist(quakes$depth)
+hist(quakes$mag)
+boxplot(quakes$depth, main = ' Single Boxplot for depthtitude')
+boxplot(quakes[,1:4], main= ' Multiple boxplot',col = 'red')
 
+var(quakes$depth, na.rm = T)
 
+skewness(quakes)
+
+kurtosis(quakes$depth)
+
+moments::skewness(quakes)
+sd(quakes$depth)
+sd(quakes$mag)
+summary(quakes)
+
+pairs(quakes)
